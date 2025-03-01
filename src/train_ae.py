088ec6489c5e3341b4c2ac49d058836005d89513
Zeheng Xiao <zee.siu2@gmail.com>
2025-03-01 16:13:20 +1100

    changes

--- src/train_ae.py
+++ src/train_ae.py
@@ -1,7 +1,8 @@
-
+import argparse
 import os
 import sys
 from pathlib import Path
+from multiprocessing import freeze_support
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[1]  # root directory
@@ -16,59 +17,81 @@ import src.dataset as dataset
 from src.data_collator_ae import DataCollatorForAttributeExtraction
 
 
+def train(pretrained_path, ds_path, config, output_dir):
+    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
+    roberta = AutoModel.from_pretrained("roberta-base")
+    roberta_config = roberta.config
 
-tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-roberta = AutoModel.from_pretrained("roberta-base")
-roberta_config = roberta.config
 
+    roberta_config_dict = roberta_config.to_dict()
+    roberta_config_dict["_name_or_path"] = "domlm"
+    roberta_config_dict["architectures"] = ["DOMLMForMaskedLM"]
+    domlm_config = model.DOMLMConfig.from_dict(roberta_config_dict)
+    # domlm_config.save_pretrained("../domlm-config/")
+    # domlm = model.DOMLMForTokenClassification(domlm_config)
+    domlm = model.DOMLMForTokenClassification.from_pretrained(pretrained_path, config=domlm_config)
 
-roberta_config_dict = roberta_config.to_dict()
-roberta_config_dict["_name_or_path"] = "domlm"
-roberta_config_dict["architectures"] = ["DOMLMForMaskedLM"]
-domlm_config = model.DOMLMConfig.from_dict(roberta_config_dict)
-# domlm_config.save_pretrained("../domlm-config/")
-domlm = model.DOMLMForTokenClassification(domlm_config)
+    state_dict = OrderedDict((f"domlm.{k}",v) for k,v in roberta.state_dict().items())
+    domlm.load_state_dict(state_dict,strict=False)
 
-state_dict = OrderedDict((f"domlm.{k}",v) for k,v in roberta.state_dict().items())
-domlm.load_state_dict(state_dict,strict=False)
+    # dataset_path = ROOT / "data/swde_preprocessed"
+    dataset_path = Path(ds_path)
 
-dataset_path = ROOT / "data/swde_preprocessed"
-print(f"Loading datasets from {dataset_path}...")
-train_ds = dataset.SWDEDataset(dataset_path)
-test_ds = dataset.SWDEDataset(dataset_path,split="test")
+    print(f"Loading datasets from {dataset_path}...")
+    train_ds = dataset.SWDEDataset(dataset_path)
+    test_ds = dataset.SWDEDataset(dataset_path,split="test")
 
-# tokenizer.pad_token = tokenizer.eos_token # why do we need this?
-data_collator = DataCollatorForAttributeExtraction(tokenizer=tokenizer, mlm_probability=0.15)
+    # tokenizer.pad_token = tokenizer.eos_token # why do we need this?
+    data_collator = DataCollatorForAttributeExtraction(tokenizer=tokenizer, mlm_probability=0.15)
 
-# install apex:
-# comment lines 32-40 in apex/setup.py
-# pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+    # install apex:
+    # comment lines 32-40 in apex/setup.py
+    # pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./
 
-#TODO: add evaluation metrics (ppl, etc.)
-training_args = TrainingArguments(
-    output_dir="./results_ae",
-    evaluation_strategy="steps",
-    # optim="adamw_apex_fused", # only with apex installed
-    weight_decay=0.01,
-    num_train_epochs=5,
-    warmup_ratio=0.1,
-    learning_rate=1e-4,
-    per_device_train_batch_size=16,
-    per_device_eval_batch_size=16,
-    gradient_accumulation_steps=4,
-    # gradient_checkpointing=True, # vram is enough without checkpointing on A4000
-    bf16 = True, # If not Ampere: fp16 = True
-    tf32 = True, # Ampere Only
-    dataloader_num_workers=8,
-    dataloader_pin_memory=True
-)
+    #TODO: add evaluation metrics (ppl, etc.)
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        evaluation_strategy="steps",
+        # optim="adamw_apex_fused", # only with apex installed
+        weight_decay=0.01,
+        num_train_epochs=5,
+        warmup_ratio=0.1,
+        learning_rate=1e-4,
+        per_device_train_batch_size=16,
+        per_device_eval_batch_size=16,
+        gradient_accumulation_steps=4,
+        # gradient_checkpointing=True, # vram is enough without checkpointing on A4000
+        bf16 = True, # If not Ampere: fp16 = True
+        tf32 = True, # Ampere Only
+        dataloader_num_workers=8,
+        dataloader_pin_memory=True
+    )
 
-trainer = Trainer(
-    model=domlm,
-    args=training_args,
-    train_dataset=train_ds,
-    eval_dataset=test_ds,
-    data_collator=data_collator,
-)
+    trainer = Trainer(
+        model=domlm,
+        args=training_args,
+        train_dataset=train_ds,
+        eval_dataset=test_ds,
+        data_collator=data_collator,
+    )
 
-trainer.train()
+    trainer.train()
+
+if __name__ == '__main__':
+    freeze_support()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pretrained_path', type=str, default='', help='preprocess data for tasks')
+    parser.add_argument('--ds_path', type=str, default='', help='data directory')
+    parser.add_argument('--config', type=str, default='domlm-config/config.json', help='config file')
+    parser.add_argument('--output_dir', type=str, default='data/preprocessed_ae', help='output directory')
+    args = parser.parse_args()
+
+    pretrained_path = args.pretrained_path
+    ds_path = args.ds_path
+    config = args.config
+    output_dir = args.output_dir
+
+    train(pretrained_path, ds_path, config, output_dir)
+    # trainer.train(resume_from_checkpoint=False, input_dir)
+
